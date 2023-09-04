87984cc614432a5c8253bbbdef852caf59caa272
tinakhodabakhshi <128822945+tinakhodabakhshi@users.noreply.github.com>
2023-09-04 16:56:35 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ButtonComponent from "./ButtonComponent";
+import ImageComponent from "./ImageComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ButtonComponent text="Clicca qui" />
+        <ImageComponent
+          src="https://media.istockphoto.com/id/1219431469/it/foto/ragazzo-che-gioca-sul-cellulare-sul-divano.jpg?s=612x612&w=0&k=20&c=m00JLeEW_pmgTSicNyue-Og7PP-E_FYNKaqZiu8QqIY="
+          alt=""
+        />
       </header>
     </div>
   );
